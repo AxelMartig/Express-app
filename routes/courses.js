0019c496222ff0edc8300c989952125b3f555db5
Axel Martig <axel.martig@reseau.eseo.fr>
2020-11-17 18:14:21 +0100

    Add 3 new courses

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const courses = [
   { id: 1, name: 'course1' },  
   { id: 2, name: 'course2' },  
-  { id: 3, name: 'course3' },  
+   { id: 3, name: 'course3' }, 
+   { id: 4, name: 'course4' },
+   { id: 5, name: 'course5' },
+   { id: 6, name: 'course6' },
 ];
 
 router.get('/', (req, res) => {
